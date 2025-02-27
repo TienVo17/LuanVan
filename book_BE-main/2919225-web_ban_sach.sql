a2c5efb64081fec94b7bfa947ed17f7ac7d647ac
Tiến Võ Văn <tienvovan917@gmail.com>
2025-02-27 01:56:17 +0000

    Update query `web_ban_sach`

--- book_BE-main/2919225-web_ban_sach.sql
+++ book_BE-main/2919225-web_ban_sach.sql
@@ -1,1 +1,3 @@
-select * from danhgia
+-- Thêm một đánh giá mới vào bảng danhgia
+INSERT INTO danhgia (id_danhgia, id_khachhang, id_sach, noidung, sosao, ngaydanhgia)
+VALUES (NULL, 1, 1, 'Sách rất hay và bổ ích', 5, NOW());
