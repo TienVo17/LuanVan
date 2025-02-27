c784d30840bb1314ebf16ef073abfbf3d685f4b0
Tiến Võ Văn <tienvovan917@gmail.com>
2025-02-27 01:56:56 +0000

    Deleted 1 query

--- book_BE-main/2919225-web_ban_sach.sql
+++
@@ -1,3 +0,0 @@
--- Thêm một đánh giá mới vào bảng danhgia
-INSERT INTO danhgia (id_danhgia, id_khachhang, id_sach, noidung, sosao, ngaydanhgia)
-VALUES (NULL, 1, 1, 'Sách rất hay và bổ ích', 5, NOW());
